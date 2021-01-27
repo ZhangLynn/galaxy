7dc2735afca5ac428f03309386d28c7ad8df859b
lynn <zl8312070@sina.com>
2021-01-27 23:23:32 +0800

    chore: less支持

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,4 @@
+const path = require('path')
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -7,5 +8,19 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ]
+  ],
+	"webpackFinal": async (defaultConfig, { configType }) => {
+		defaultConfig.module.rules.push(
+			{
+				test: /\.less$/,
+				loaders: ['style-loader', 'css-loader', 'less-loader'],
+				include: path.resolve(__dirname, '../src')
+			}
+		)
+		defaultConfig.module.rules.map(rule => {
+			console.log('<----' + JSON.stringify(rule) + '---->')
+		})
+
+		return defaultConfig
+	},
 }
